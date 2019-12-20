1466340d513e819799e59127aeeb94118a44283d
Dmitry Barskov <barskoidim@gmail.com>
2019-12-20 16:38:06 +0300

    Refactor Array#group

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,28 +1,19 @@
+require_relative "groups"
+
 class Sequence
+  using Groups
+
   def initialize(str)
     @digits = str.chars
   end
 
   def next
-    new_digits = @digits.group.flat_map { |g| [g.length, g.first] }
+    new_digits = @digits.consecutive_eql_groups
+                        .flat_map { |g| [g.length, g.first] }
     Sequence.new new_digits.join
   end
 
   def to_s
     @digits.join
-  end
-end
-
-class Array
-  def group
-    groups = []
-    each do |element|
-      if groups.last && element == groups.last.first
-        groups.last << element
-      else
-        groups << [element]
-      end
-    end
-    groups
   end
 end
